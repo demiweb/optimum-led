17e1f97716fe5cac0bec5cf5d405e0b6c0995c54
yurayarosh <yura12323@gmail.com>
2019-06-27 18:03:56 +0300

    some edits

--- src/js/components/animations/setContatsPage.js
+++ src/js/components/animations/setContatsPage.js
@@ -1,13 +1,20 @@
 import anime from 'animejs';
-import { SECTION_BLOCK, SECTION_BG, FLASH_IN, ANIMATE, FLASH_DURATION } from '../../constants';
+import { SECTION_BLOCK, SECTION_BG, FLASH_IN, ANIMATE, FLASH_DURATION, TITLE } from '../../constants';
 
 export default function setContactsPage() {
   const block = document.querySelector(`.${SECTION_BLOCK}`);
   const img = document.querySelector(`.${SECTION_BG}`);
   const word = document.querySelector(`.${FLASH_IN}`);
+  const title = document.querySelector(`.${TITLE}`);
 
   if (word) {
     word.classList.add(ANIMATE);
+  };
+
+  if (title) {
+    setTimeout(() => {
+      title.classList.add(ANIMATE);
+    }, FLASH_DURATION);
   };
 
   const tl = anime.timeline({ 'easing': 'linear' });
